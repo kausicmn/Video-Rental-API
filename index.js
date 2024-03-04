8a7b65bac8f7e4d788fbc6f89aa8f0e0a474d4fa
kausicmn <92553246+kausicmn@users.noreply.github.com>
2024-03-03 22:07:14 -0500

    Added Movies and rentals api

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var express=require('express');
 const mongoose=require('mongoose');
+const Joi=require('joi')
+Joi.objectId=require('joi-objectid')(Joi)
 const app=express()
 const Joi=require('joi');
 const genres=require('./routes/genres')
+const movies=require('./routes/movies')
 const customer=require('./routes/customer')
+const rental=require('./routes/rentals')
 mongoose.connect('mongodb://localhost/video-rental-api').then(()=>console.log('Connected to MongoDB...')).catch((err)=>console.log(err))
 app.use(express.json())
 app.use('/api/genres',genres);
 app.use('/api/customer',customer)
+app.use('/api/movies',movies)
+app.use('/api/rentals',rental)
 const port=process.env.PORT||3000
 app.listen(port,()=>console.log(`Listening on Port ${port}...`));
 
--- models/genres.js
+++ models/genres.js
@@ -7,10 +7,9 @@ const Genre=mongoose.model('Genre',genreSchema);
 function validategenre(genre){
     const schema=Joi.object({
         name:Joi.string().min(5).required(),
-        phone:Joi.string().min(5).required(),
-        isGold:Joi.boolean()
     })
     return schema.validate(genre)
 }
 exports.Genre=Genre
+exports.genreSchema=genreSchema
 exports.validate=validategenre
